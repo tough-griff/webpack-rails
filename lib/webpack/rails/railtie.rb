0b029ba62f4cf54ede19af20d683944d69f3d115
Griffin Yourick <gryphon92@gmail.com>
2016-07-23 13:00:19 -0400

    Fix railtie

--- lib/webpack/rails/railtie.rb
+++ lib/webpack/rails/railtie.rb
@@ -6,18 +6,18 @@ module Webpack
     class Railtie < ::Rails::Railtie
       config.webpack = ActiveSupport::OrderedOptions.new
       # Sensible defaults
-      config.webpack.host_name           = host_name           = "localhost"
-      config.webpack.hot_loading_enabled = hot_loading_enabled = true
-      config.webpack.node_port           = node_port           = 4000
-      config.webpack.simulate_production = simulate_production = false
-      config.webpack.suffix              = suffix              = "bundle"
+      config.webpack.host_name           = "localhost"
+      config.webpack.hot_loading_enabled = true
+      config.webpack.node_port           = 4000
+      config.webpack.simulate_production = false
+      config.webpack.suffix              = "bundle"
 
       rake_tasks do
         load File.expand_path("../../../tasks/build.rake", __FILE__)
       end
 
       # Configure (pre)compilation
-      initializer "webpack_rails.configure_precompilation", group: :all do |app|
+      initializer "webpack_rails.configure_precompilation", after: :load_config_initializers, group: :all do |app|
         app.config.assets.precompile += [
           /.*\.#{app.config.webpack.suffix}\.(css|js)$/,
         ]
@@ -28,17 +28,21 @@ module Webpack
       end
 
       # Configure Hot Loader
-      initializer "webpack_rails.configure_hot_loader", group: :all do |app|
-        if hot_loading_enabled && ::Rails.env.development?
-          if simulate_production
-            app.config.action_controller.asset_host = proc do |source|
-              "//#{host_name}:#{node_port}/assets" if source.ends_with?("#{suffix}.js")
-            end
-          else
+      initializer "webpack_rails.configure_hot_loader", after: :load_config_initializers, group: :all do |app|
+        if app.config.webpack.hot_loading_enabled && ::Rails.env.development?
+          if app.config.webpack.simulate_production
             # In a production-like environment, pull assets straight from public/assets.
             config.assets.compile     = false
             config.assets.debug       = false
             config.serve_static_files = true
+          else
+            host_name = app.config.webpack.host_name
+            node_port = app.config.webpack.node_port
+            suffix    = app.config.webpack.suffix
+
+            app.config.action_controller.asset_host = proc do |source|
+              "//#{host_name}:#{node_port}/assets" if source.ends_with?("#{suffix}.js")
+            end
           end
         end
       end
